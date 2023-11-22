c87baada9b165fa9aacc92a3d7344ee4c2d88402
Jana Maire <s0563285@htw-berlin.de>
2023-11-22 19:41:21 +0100

    Imprint-Komponente mit ersten Inhalten eingebunden

--- htw-antidiskriminierungsapp/src/app/app-routing.module.ts
+++ htw-antidiskriminierungsapp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component:HomeComponent, pathMatch: 'full'},
+  {path: 'imprint', component: ImprintComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
