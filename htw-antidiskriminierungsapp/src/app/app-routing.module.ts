43a80f4e0ad16aa54201ed29b0f1dd9d8ec1eeb7
Jana Maire <s0563285@htw-berlin.de>
2023-11-27 18:58:35 +0100

    Impressum mit Home-Button eingefügt

--- htw-antidiskriminierungsapp/src/app/app-routing.module.ts
+++ htw-antidiskriminierungsapp/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 import { PrivacypolicyComponent } from './privacypolicy/privacypolicy.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: "", component:HomeComponent, pathMatch: 'full'},
+  {path: "", component:HomeComponent},
   {path: 'imprint', component: ImprintComponent},
+  {path: 'impressum', component: ImpressumComponent},
   {path: 'privacypolicy', component:PrivacypolicyComponent},
 ];
 
--- htw-antidiskriminierungsapp/src/app/app.module.ts
+++ htw-antidiskriminierungsapp/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { PrivacypolicyComponent } from './privacypolicy/privacypolicy.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PrivacypolicyComponent } from './privacypolicy/privacypolicy.component'
     FooterComponent,
     HomeComponent,
     ImprintComponent,
-    PrivacypolicyComponent
+    PrivacypolicyComponent,
+    ImpressumComponent
   ],
   imports: [
     BrowserModule,
